6b4045d2de8d9dd55110fdb7169ab1715e749f2a
LeulBerhanu <leulberhanua@gmail.com>
2024-04-02 16:53:46 +0300

    router and homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import CreatePost from "../components/Posts/CreatePost";
 import PostsList from "../components/Posts/PostsList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PublicNavbar from "../components/Navbar/PublicNavbar";
+import HomePage from "../Pages/HomePage";
 
 function App() {
   return (
-    <div>
-      <CreatePost />
-      <PostsList />
-    </div>
+    <BrowserRouter>
+      <PublicNavbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/posts" element={<PostsList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
