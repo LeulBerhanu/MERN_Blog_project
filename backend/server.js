a630f052fdd1c7e1f97fe112190393eeb036cdc2
LeulBerhanu <leulberhanua@gmail.com>
2024-04-03 13:49:01 +0300

    express router

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const asyncHandler = require("express-async-handler");
 const cors = require("cors");
 const Post = require("./models/Post/Post");
 const connectDB = require("./utils/connectDB");
+const postRouter = require("./router/post/postsRouter");
 
 // Connect DB
 connectDB();
@@ -22,67 +23,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.post(
-  "/api/v1/posts/create",
-  asyncHandler(async (req, res) => {
-    const { title, description } = req.body;
-
-    // find post by title
-    const postFound = await Post.findOne({ title });
-    if (postFound) {
-      throw new Error("Post already exists");
-    }
-
-    const postCreated = await Post.create({ title, description });
-    res
-      .status(200)
-      .json({ message: "Post created successfully!", data: postCreated });
-  })
-);
-
-app.get(
-  "/api/v1/posts",
-  asyncHandler(async (req, res) => {
-    const posts = await Post.find();
-    res.status(200).json(posts);
-  })
-);
-
-app.put(
-  "/api/v1/posts/:postId",
-  asyncHandler(async (req, res) => {
-    const postId = req.params.postId;
-
-    const postFound = await Post.findById(postId);
-    if (!postFound) {
-      throw new Error("Post not found");
-    }
-
-    const postUpdated = await Post.findByIdAndUpdate(postId, req.body, {
-      new: true,
-    });
-
-    res.status(200).json(postUpdated);
-  })
-);
-
-app.get(
-  "/api/v1/posts/:postId",
-  asyncHandler(async (req, res) => {
-    const postId = req.params.postId;
-    const post = await Post.findById(postId);
-    res.status(200).json(post);
-  })
-);
-
-app.delete(
-  "/api/v1/posts/:postId",
-  asyncHandler(async (req, res) => {
-    const postId = req.params.postId;
-    const deletePost = await Post.findByIdAndDelete(postId);
-    res.status(200).json(deletePost);
-  })
-);
+app.use("/api/v1/posts", postRouter);
 
 // Not found handler
 app.use((req, res, next) => {
