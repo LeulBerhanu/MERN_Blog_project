2c341708eec2bc99b25eee7d11374acbce394d72
LeulBerhanu <leulberhanua@gmail.com>
2024-04-04 18:09:00 +0300

    public navbar and homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,13 @@ import PostsList from "../components/Posts/PostsList";
 import UpdatePost from "../components/Posts/UpdatePost";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PublicNavbar from "../components/Navbar/PublicNavbar";
-import HomePage from "../Pages/HomePage";
-
+import Home from "../Pages/Home/Home";
 function App() {
   return (
     <BrowserRouter>
       <PublicNavbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
         <Route path="/posts" element={<PostsList />} />
         <Route path="/posts/:postId" element={<UpdatePost />} />
