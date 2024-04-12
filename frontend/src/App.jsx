a70a8c107cc56a62cde871ec2561784c9a425fad
LeulBerhanu <leulberhanua@gmail.com>
2024-04-12 22:43:09 +0300

    private navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import CreatePost from "../components/Posts/CreatePost";
 import PostsList from "../components/Posts/PostsList";
 import Login from "../Pages/User/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PublicNavbar from "../components/Navbar/PublicNavbar";
+import PrivateNavbar from "../components/Navbar/PrivateNavbar";
 import Home from "../Pages/Home/Home";
 import PostDetails from "../components/Posts/PostDetails";
 import Register from "../Pages/User/Register";
 import Profile from "../Pages/User/Profile";
+import { useSelector, useDispatch } from "react-redux";
+import { useQuery } from "@tanstack/react-query";
+import { isAuthenticated } from "./redux/slices/authSlices";
+import { checkAuthStatusAPI } from "./APIServices/user/userAPI";
+
 function App() {
+  const { isError, isLoading, data, error, isSuccess, refetch } = useQuery({
+    queryKey: ["user-auth"],
+    queryFn: checkAuthStatusAPI,
+  });
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(isAuthenticated(data));
+  }, [data]);
+
+  const { userAuth } = useSelector((state) => state.auth);
+
   return (
     <BrowserRouter>
-      <PublicNavbar />
+      {userAuth ? <PrivateNavbar /> : <PublicNavbar />}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
